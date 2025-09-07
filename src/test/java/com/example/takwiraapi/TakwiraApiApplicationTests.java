8eb51e36ef816f39439a632c42ec7cb766908826
adamsaidane <adam.adamm.184@gmail.com>
2025-09-07 18:18:28 +0100

    init

---
+++ src/test/java/com/example/takwiraapi/TakwiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takwiraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakwiraApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
