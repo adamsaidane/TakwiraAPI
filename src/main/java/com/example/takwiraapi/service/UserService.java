bf821f9dcc4c038a00a5449bb941796e9a10415d
adamsaidane <adam.adamm.184@gmail.com>
2025-09-15 17:14:44 +0100

    Added Login System

---
+++ src/main/java/com/example/takwiraapi/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.takwiraapi.service;
+
+import com.example.takwiraapi.entity.User;
+import com.example.takwiraapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+}
+
